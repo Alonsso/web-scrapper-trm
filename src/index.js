9f713769b83fb01df5f98d4809e1a2a1d1cb9e50
Alonso <cardenasjairods@gmail.com>
2022-07-23 17:18:58 -0500

    first scrape using superfinanciera iframe src

--- src/index.js
+++ src/index.js
@@ -5,7 +5,24 @@ const puppeteer = require('puppeteer');
         headless: false,
     });
     const page = await browser.newPage();
-    await page.goto('https://www.bvc.com.co/pps/tibco/portalbvc');
-    await page.screenshot({path: 'bvc.png'});
+    await page.goto('https://www.superfinanciera.gov.co/CargaDriver/index.jsp')
+        .then(() => {
+
+        }).catch();
+    await page.screenshot({ path: 'bvc.png' });
+    await page.setExtraHTTPHeaders({
+        "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+        "upgrade-insecure-requests": "1",
+        "accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9,en;q=0.8",
+    });
+    await page.waitForSelector("table");
+    const value = await page.$$eval("table tr td", tables => tables.map((table)=>{
+        return table.innerText.trim();
+    }));
+    console.log("value : ", value.join(' '));
     await browser.close();
 })();
